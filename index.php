35a10871ecaf068bd29d867bef259d573edd29ce
Heiner Lohaus <heiner.lohaus@avency.de>
2020-02-05 14:12:09 +0100

    Update index.php to master

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 <?php
 
-use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Exception\ConnectionException;
 use PackageVersions\Versions;
+use Shopware\Core\Framework\Event\BeforeSendResponseEvent;
+use Shopware\Core\Framework\Adapter\Cache\CacheIdLoader;
 use Shopware\Core\Framework\Plugin\KernelPluginLoader\DbalKernelPluginLoader;
 use Shopware\Core\Framework\Routing\RequestTransformerInterface;
-use Shopware\Core\Framework\Uuid\Uuid;
 use Shopware\Development\Kernel;
 use Shopware\Storefront\Framework\Cache\CacheStore;
-use Shopware\Storefront\Framework\Csrf\CsrfPlaceholderHandler;
 use Symfony\Component\Debug\Debug;
 use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\HttpFoundation\Request;
@@ -24,8 +23,8 @@ if (!isset($_SERVER['APP_ENV']) && !isset($_ENV['APP_ENV'])) {
     (new Dotenv(true))->load(__DIR__.'/private/.env');
 }
 
-$env = $_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? 'dev';
-$debug = (bool) ($_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? ('prod' !== $env));
+$appEnv = $_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? 'dev';
+$debug = (bool) ($_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? ('prod' !== $appEnv));
 
 if ($debug) {
     umask(0000);
@@ -41,28 +40,27 @@ if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false
     Request::setTrustedHosts(explode(',', $trustedHosts));
 }
 
+if (class_exists('Shopware\Core\HttpKernel')) {
+    $request = Request::createFromGlobals();
+
+    $kernel = new \Shopware\Development\HttpKernel($appEnv, $debug, $classLoader);
+    $result = $kernel->handle($request);
+
+    $result->getResponse()->send();
+
+    $kernel->terminate($result->getRequest(), $result->getResponse());
+
+    return;
+}
+
 // resolve SEO urls
 $request = Request::createFromGlobals();
 $connection = Kernel::getConnection();
 
-if ($env === 'dev') {
+if ($appEnv === 'dev') {
     $connection->getConfiguration()->setSQLLogger(
         new \Shopware\Core\Profiling\Doctrine\DebugStack()
     );
-}
-
-function getCacheId(Connection $connection): string
-{
-    try {
-        $cacheId = $connection->fetchColumn(
-            'SELECT `value` FROM app_config WHERE `key` = :key',
-            ['key' => 'cache-id']
-        );
-    } catch (\Exception $e) {
-        return Uuid::randomHex();
-    }
-
-    return $cacheId ?? Uuid::randomHex();
 }
 
 try {
@@ -70,29 +68,33 @@ try {
 
     $pluginLoader = new DbalKernelPluginLoader($classLoader, null, $connection);
 
-    $cacheId = getCacheId($connection);
+    $cacheId = (new CacheIdLoader($connection))
+        ->load();
 
-    $kernel = new Kernel($env, $debug, $pluginLoader, $cacheId, $shopwareVersion, $connection);
+    $kernel = new Kernel($appEnv, $debug, $pluginLoader, $cacheId, $shopwareVersion, $connection);
     $kernel->boot();
 
+    $container = $kernel->getContainer();
+
     // resolves seo urls and detects storefront sales channels
-    $request = $kernel->getContainer()
+    $request = $container
         ->get(RequestTransformerInterface::class)
         ->transform($request);
 
-    $csrfTokenHelper = $kernel->getContainer()->get(CsrfPlaceholderHandler::class);
-
-    $enabled = $kernel->getContainer()->getParameter('shopware.http.cache.enabled');
+    $enabled = $container->getParameter('shopware.http.cache.enabled');
     if ($enabled) {
-        $store = $kernel->getContainer()->get(CacheStore::class);
+        $store = $container->get(CacheStore::class);
 
         $kernel = new HttpCache($kernel, $store, null, ['debug' => $debug]);
     }
 
     $response = $kernel->handle($request);
 
-    // replace csrf placeholder with fresh tokens
-    $response = $csrfTokenHelper->replaceCsrfToken($response);
+    $event = new BeforeSendResponseEvent($request, $response);
+    $container->get('event_dispatcher')
+        ->dispatch($event);
+
+    $response = $event->getResponse();
 
 } catch (ConnectionException $e) {
     throw new RuntimeException($e->getMessage());
